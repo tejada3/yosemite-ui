aa232c114af1e1570b1362c4732dbda89d529e09
Colby Wall <colby.wall@hitachivantara.com>
2022-03-22 09:06:19 -0500

    Presigned post attempt

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Theme } from "@mui/material";
 import axios from "axios";
-import ImageCard from "./ImageCard"
 import List from "./List";
 import background from "../../static/RbackV2.jpg"
-import background2 from "../../static/a3aaa2bfbe3ddd78a284aa14f5b942ec.jpg"
 
 
 const Gallery = () => {
@@ -35,19 +33,52 @@ const Gallery = () => {
     
     const classes = useStyles();
 
-    const onFileInput = (event: any) => {
-        if (event.target.files && event.target.files[0]) {
+    const onFileInput = async (event: any) => {
 
-            console.log(event.target.files[0]);
-            //Upload here
+        const presignedPostResponse = await axios.get(`https://l3yu0l18ib.execute-api.us-east-1.amazonaws.com/Yosemite/s3?method=post&image_name=${event.target.files[0].name}`);
+       
+        console.log(presignedPostResponse);
+        if (event.target.files && event.target.files[0]) {
+            let payload = presignedPostResponse.data.payload.url
+            let fields = presignedPostResponse.data.payload.fields
+
+            //formData.append('file', event.target.files[0]); // The file must be the last element
+          
+            // console.log(fields);
+
+            // console.log(payload)
+            
+            // const response = await fetch(payload, {
+            //     method: 'POST',
+            //     headers: {
+            //         "Access-Control-Allow-Headers" : "Content-Type",
+            //         "Content-Type": "application/json",
+            //         "Access-Control-Allow-Origin":  "*",
+            //      },
+            //     body: {
+            //         fields,
+            //         file: event.target.files[0]
+            //     }
+            //});
+
+            // ,{
+            //     headers: {}, 
+            //     data: fields
+            //   });
+            
+            let response = await axios.post(presignedPostResponse.data.payload.url,{
+                data: fields,
+                file: event.target.files[0]
+            });
+
+            console.log("POST RESPONSE: ",response);
         }
     }
 
     async function preSignedUrl() {
-        const response = await axios.get('https://l3yu0l18ib.execute-api.us-east-1.amazonaws.com/Yosemite/s3');
+        const response = await axios.get('https://l3yu0l18ib.execute-api.us-east-1.amazonaws.com/Yosemite/s3?method=get');
+        console.log(response);
         setImageList(response.data.payload);
-        console.log(response.data.payload);
-        return response.data.payload;
     }
 
     return <>
@@ -63,10 +94,10 @@ const Gallery = () => {
                 >
                 Upload Image
                 <input
+                    id="file-input"
                     type="file"
                     hidden
                     onChange={onFileInput}
-
                 />
                 </Button>
             </div>
